cda154bc12737c4de0725d7bfe6281aaddb9cf4b
p.gabrieldeveloper <paulo.gabriel@atoscapital.com.br>
2022-04-23 12:36:04 -0300

    feat adicionando função de call back

--- str-producer/src/main/java/com/paulo/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/paulo/strproducer/services/StringProducerService.java
@@ -1,17 +1,29 @@
 package com.paulo.strproducer.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class StringProducerService {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message){
-        kafkaTemplate.send("str-topic", message);
+        kafkaTemplate.send("str-topic", message).addCallback(
+                success -> {
+                    if(success != null){
+                        log.info("Menssagem enviado com sucesso");
+                        log.info("Partition: {} OffSet: {}", success.getRecordMetadata().partition(), success.getRecordMetadata().partition());
+                    }
+                },
+                error -> {
+                    log.error("Error ao enviar menssagem: {}", error.getMessage());
+                }
+        );
     }
 
 }
