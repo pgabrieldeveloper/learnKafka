b095c4381d4b33ce9f7b1ddf92163bb633026630
p.gabrieldeveloper <paulo.gabriel@atoscapital.com.br>
2022-04-24 09:13:06 -0300

    refactor escolhendo qual partição cada função vai escutar

--- str-cosumer/src/main/java/com/paulo/strcosumer/listeners/StrConsumerListner.java
+++ str-cosumer/src/main/java/com/paulo/strcosumer/listeners/StrConsumerListner.java
@@ -1,2 +1,28 @@
-package com.paulo.strcosumer.listeners;public class StrConsumerListner {
+package com.paulo.strcosumer.listeners;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class StrConsumerListner {
+
+
+    @KafkaListener(groupId = "group-1", topicPartitions = {@TopicPartition(topic = "str-topic", partitions = {"1"})} , containerFactory = "strContainerFactory")
+    public void crete(String message){
+        log.info("CREATED ::: Menssagem Recebida: {}", message);
+    }
+
+    @KafkaListener(groupId = "group-1", topicPartitions = {@TopicPartition(topic = "str-topic", partitions = {"0"})}, containerFactory = "strContainerFactory")
+    public void log(String message){
+        log.info("LOG :::: Menssagem Recebida: {}", message);
+    }
+
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void outro(String message){
+        log.info("OUTRO :::: Menssagem Recebida: {}", message);
+    }
+
 }
---
+++ str-cosumer/src/test/java/com/paulo/strcosumer/StrCosumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulo.strcosumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrCosumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
